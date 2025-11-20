c40c62ba80bacb2be689ae46b6e12c76e77dd025
SH Woo <imdlsrks.mc@gmail.com>
2025-11-20 11:05:14 +0900

    fix: 테스트 환경 수정

--- src/test/java/com/kakaotechbootcamp/community/config/TestConfig.java
+++ src/test/java/com/kakaotechbootcamp/community/config/TestConfig.java
@@ -1,7 +1,10 @@
 package com.kakaotechbootcamp.community.config;
 
+import com.kakaotechbootcamp.community.infra.image.ImageSignature;
+import com.kakaotechbootcamp.community.infra.image.ImageStorageService;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -15,5 +18,18 @@ public class TestConfig {
     @Bean
     public DateTimeProvider testDateTimeProvider() {
         return () -> Optional.of(Instant.now());
+    }
+
+    @Bean
+    @Primary
+    public ImageStorageService testImageStorageService() {
+        return type -> new ImageSignature(
+                "test-api-key",
+                "test-cloud-name",
+                System.currentTimeMillis() / 1000L,
+                "test-signature",
+                "test-upload-preset",
+                "profile".equals(type) ? "profiles" : "posts"
+        );
     }
 }
