66deccf0132fd56c7eea552d69461bd5fa20ab99
SH Woo <imdlsrks.mc@gmail.com>
2025-11-20 16:09:03 +0900

    feat: oauth 성공 헨들러 등록

---
+++ src/main/java/com/kakaotechbootcamp/community/application/security/handler/OAuthSuccessHandler.java
@@ -0,0 +1,20 @@
+package com.kakaotechbootcamp.community.application.security.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+public class OAuthSuccessHandler implements AuthenticationSuccessHandler {
+
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+
+        // todo : Cookie로 refresh 토큰 전달
+
+    }
+}
