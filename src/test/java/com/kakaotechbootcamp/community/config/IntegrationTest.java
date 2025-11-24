0ecb8c156de30d1eca48a21b082dc764c4867c75
SH Woo <imdlsrks.mc@gmail.com>
2025-11-24 17:41:25 +0900

    feat: 통합테스트 어노테이션 수정

--- src/test/java/com/kakaotechbootcamp/community/config/IntegrationTest.java
+++ src/test/java/com/kakaotechbootcamp/community/config/IntegrationTest.java
@@ -9,11 +9,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
+/**
+ * 전체 애플리케이션 컨텍스트(SpringBootTest)를 띄워 Controller → Service → Repository →
+ * DB 흐름과 Security/Infrastructure를 포함한 실제 통합 시나리오를 검증하기 위한 테스트 어노테이션.
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@SpringBootTest
 @ActiveProfiles("test")
 @Import(TestConfig.class)
+@SpringBootTest
 @Tag("integration")
 public @interface IntegrationTest {
 }
