dbabd3fb2808c4788ce0bb7d5e1f45e0388aa98d
SH Woo <imdlsrks.mc@gmail.com>
2025-11-26 10:14:51 +0900

    feat: 가짜 객체 생성

--- src/test/java/com/kakaotechbootcamp/community/config/ImageStorageMockConfig.java
+++ src/test/java/com/kakaotechbootcamp/community/config/ImageStorageMockConfig.java
@@ -1,5 +1,6 @@
 package com.kakaotechbootcamp.community.config;
 
+import com.kakaotechbootcamp.community.fake.FakeImageStorageService;
 import com.kakaotechbootcamp.community.infra.image.ImageSignature;
 import com.kakaotechbootcamp.community.infra.image.ImageStorageService;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -8,23 +9,14 @@ import org.springframework.context.annotation.Primary;
 
 /**
  * 통합 테스트에서 실제 Cloudinary 등 외부 이미지 스토리지를 호출하지 않도록
- * 고정된 서명/타임스탬프를 반환하는 테스트용 ImageStorageService 설정
+ * 고정된 서명/타임스탬프를 반환하는 테스트용 FakeImageStorageService 등록
  */
 @TestConfiguration
 public class ImageStorageMockConfig {
 
-    private static final long FIXED_TIMESTAMP = 1735689600L;
-
     @Bean
     @Primary
     public ImageStorageService testImageStorageService() {
-        return type -> new ImageSignature(
-                "test-api-key",
-                "test-cloud-name",
-                FIXED_TIMESTAMP,
-                "test-signature",
-                "test-upload-preset",
-                "profile".equals(type) ? "profiles" : "posts"
-        );
+        return new FakeImageStorageService();
     }
 }
