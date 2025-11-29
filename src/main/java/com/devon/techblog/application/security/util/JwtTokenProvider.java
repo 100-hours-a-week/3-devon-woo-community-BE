03a4d4ce2cae5c16bf713a260ffdf9795ebcbb7e
SH Woo <imdlsrks.mc@gmail.com>
2025-11-29 16:58:31 +0900

    ♻️ refactor: properties 파일 이동

--- src/main/java/com/devon/techblog/application/security/util/JwtTokenProvider.java
+++ src/main/java/com/devon/techblog/application/security/util/JwtTokenProvider.java
@@ -1,6 +1,6 @@
 package com.devon.techblog.application.security.util;
 
-import com.devon.techblog.application.security.config.JwtProperties;
+import com.devon.techblog.application.security.config.properties.JwtProperties;
 import com.devon.techblog.application.security.constants.JwtConstants;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
